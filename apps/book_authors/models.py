6a8f6693eb8994fb4b45c827001a70cb7a496edb
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-07-17 16:43:12 -0700

    added Books/Authors app

---
+++ apps/book_authors/models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+from django.db import models
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __repr__(self):
+        return "<Book object: {}>".format(self.name)
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book, related_name="authors")
+    notes = models.TextField(default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __repr__(self):
+        return "<Author objet: {} {}>".format(self.first_name, self.last_name)
